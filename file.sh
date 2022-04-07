c96d78209826b9680d028bc0ffaf712adff18f53
Ron <ront@migal.org.il>
2022-04-07 16:58:37 +0300

    trying a to disable systemctl line

--- file.sh
+++ file.sh
@@ -12,7 +12,7 @@ sudo chkconfig docker on
 sudo yum install -y git
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo systemctl enable docker
+#sudo systemctl enable docker
 #sudo docker login -u ronturetzkymigal -p derpyderp2
 #sudo docker pull ronturetzkymigal/mgpipe
 #sudo docker run -it ronturetzkymigal/mgpipe
