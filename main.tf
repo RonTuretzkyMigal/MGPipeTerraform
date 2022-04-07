d1f96872e154c32cfcc5b4efaa6423cb300758bd
Ron <ront@migal.org.il>
2022-04-07 18:16:23 +0300

    testing security group changes

--- main.tf
+++ main.tf
@@ -19,9 +19,44 @@ resource "aws_instance" "app_server" {
   instance_type   = "t2.micro"
   key_name        = "pair_test"
   user_data	= file("file.sh")
+  vpc_security_group_ids = [aws_security_group.Docker.id]
+
+
 
   tags = {
     Name = "ExampleAppServerInstance"
+  }
+
+  resource "aws_security_group" "Docker" {
+   egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+ ingress                = [
+   {
+     cidr_blocks      = [ "0.0.0.0/0", ]
+     description      = ""
+     from_port        = 22
+     ipv6_cidr_blocks = []
+     prefix_list_ids  = []
+     protocol         = "tcp"
+     security_groups  = []
+     self             = false
+     to_port          = 22
+  }
+  ]
+  }
+  tags = {
+    type = "terraform-test-security-group"
   }
 }
 
