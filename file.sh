5beee07f0770b65fdfb972ee758e2d3ec9bf2698
Ron <ront@migal.org.il>
2022-04-07 16:26:53 +0300

    completely disabling start script

--- file.sh
+++ file.sh
@@ -13,8 +13,8 @@ sudo yum install -y git
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo systemctl enable docker
-sudo docker login -u ronturetzkymigal -p derpyderp2
-sudo docker pull ronturetzkymigal/mgpipe
+#sudo docker login -u ronturetzkymigal -p derpyderp2
+#sudo docker pull ronturetzkymigal/mgpipe
 #sudo docker run -it ronturetzkymigal/mgpipe
 #sudo ./run-assembly /opt/bin/bio/SampleDir/ /home/Test1 *1.fastq.gz --verbose --ncpus=1
 
